9979382d2df6a0eb3a9d301040ee06567442656c
lyx0206331 <rq880113@163.com>
2021-12-30 18:00:19 +0800

    移除过时kotlin-android-extensions插件，增加kotlin-parcelize,引入LiveData库

--- app/src/main/java/com/adrian/test/MainActivity.kt
+++ app/src/main/java/com/adrian/test/MainActivity.kt
@@ -2,10 +2,11 @@ package com.adrian.test
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.widget.SeekBar
 import com.adrian.commlib.util.*
 import com.adrian.commlib.view.SegmentableStepsView
-import kotlinx.android.synthetic.main.activity_main.*
+import com.adrian.test.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,22 +14,23 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        tvModel.text = "型号:${getSystemModel()}\n品牌:${getDeviceBrand()}"
+        binding.tvModel.text = "型号:${getSystemModel()}\n品牌:${getDeviceBrand()}"
 
-        btnSwitch.setOnClickListener {
-            segmentableStepsView.stepStyle = when (segmentableStepsView.stepStyle) {
+        binding.btnSwitch.setOnClickListener {
+            binding.segmentableStepsView.stepStyle = when (binding.segmentableStepsView.stepStyle) {
                 SegmentableStepsView.STYLE_LINE_HORIZONTAL -> SegmentableStepsView.STYLE_LINE_VERTICAL
                 SegmentableStepsView.STYLE_LINE_VERTICAL -> SegmentableStepsView.STYLE_RING
                 SegmentableStepsView.STYLE_RING -> SegmentableStepsView.STYLE_CIRCLE
                 else -> SegmentableStepsView.STYLE_LINE_HORIZONTAL
             }
         }
-        seekbar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+        binding.seekbar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
             override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
                 "Step".logE("current progress:$progress")
-                segmentableStepsView.stepIndex = progress
+                binding.segmentableStepsView.stepIndex = progress
             }
 
             override fun onStartTrackingTouch(seekBar: SeekBar?) {
@@ -38,72 +40,72 @@ class MainActivity : AppCompatActivity() {
             }
 
         })
-        segmentableStepsView.maxSteps = seekbar.max
-        segmentableStepsView.stepIndex = seekbar.progress
-        segmentableStepsView.stepChangeListener = { step ->
+        binding.segmentableStepsView.maxSteps = binding.seekbar.max
+        binding.segmentableStepsView.stepIndex = binding.seekbar.progress
+        binding.segmentableStepsView.stepChangeListener = { step ->
             "Step".logE("current step:$step")
             if (step == 0) {
-                segmentableStepsView.ringCenterText = "步骤(0)"
+                binding.segmentableStepsView.ringCenterText = "步骤(0)"
             } else {
                 if (isAdd) {
-                    segmentableStepsView.ringCenterText += "步骤(${step})"
+                    binding.segmentableStepsView.ringCenterText += "步骤(${step})"
                 } else {
-                    val string = segmentableStepsView.ringCenterText
+                    val string = binding.segmentableStepsView.ringCenterText
                     val index = string?.indexOf("步骤(${step+1})".also { "Data".logE("delete content:$it") }).also { "Data".logE("delete index:$it") }
                     if (index!! > 0) {
-                        segmentableStepsView.ringCenterText = string?.substring(0, index)
+                        binding.segmentableStepsView.ringCenterText = string?.substring(0, index)
                     }
                 }
             }
         }
-        btnIncrease.setOnClickListener {
+        binding.btnIncrease.setOnClickListener {
             isAdd = true
-            seekbar.progress++
+            binding.seekbar.progress++
         }
-        btnDecrease.setOnClickListener {
+        binding.btnDecrease.setOnClickListener {
             isAdd = false
-            seekbar.progress--
+            binding.seekbar.progress--
         }
-        seekbar.progress = 4
+        binding.seekbar.progress = 4
 
         val fValue = 1.56f
         val dValue = -1.56
         val iValue = 1
         val iValueN = -iValue
         val fBytes = byteArrayOf(20, -82, -57, 63)
-        tvInt2Bytes.text = "LE:Int -> Bytes:$iValue -> ${iValue.toByteArrayLE().contentToString()}\n" +
+        binding.tvInt2Bytes.text = "LE:Int -> Bytes:$iValue -> ${iValue.toByteArrayLE().contentToString()}\n" +
                 "LE:Int -> Bytes:$iValueN -> ${iValueN.toByteArrayLE().contentToString()}\n" +
                 "Signed -> UnSigned:$iValueN -> ${iValueN.toUInt()}\n" +
                 "LE:Float -> Bytes:$fValue -> ${fValue.toByteArrayLE().contentToString()}\n" +
                 "LE:Bytes -> Float:${fBytes.contentToString()} -> ${fBytes.toFloatLE()}\n" +
                 "BE:Float -> Bytes:$fValue -> ${fValue.toByteArrayBE().contentToString()}"
         fBytes.reverse()
-        tvInt2Bytes.append("\nBE:Bytes -> Float:${fBytes.contentToString()} -> ${fBytes.toFloatBE()}")
+        binding.tvInt2Bytes.append("\nBE:Bytes -> Float:${fBytes.contentToString()} -> ${fBytes.toFloatBE()}")
         val dBytes = byteArrayOf(-10, 40, 92, -113, -62, -11, -8, -65)
-        tvInt2Bytes.append("\nLE:Double -> Bytes:$dValue -> ${dValue.toByteArrayLE().contentToString()}")
-        tvInt2Bytes.append("\nLE:Bytes -> Double:${dBytes.contentToString()} -> ${dBytes.toDoubleLE()}")
-        tvInt2Bytes.append("\nBE:Double -> Bytes:$dValue -> ${dValue.toByteArrayBE().contentToString()}")
+        binding.tvInt2Bytes.append("\nLE:Double -> Bytes:$dValue -> ${dValue.toByteArrayLE().contentToString()}")
+        binding.tvInt2Bytes.append("\nLE:Bytes -> Double:${dBytes.contentToString()} -> ${dBytes.toDoubleLE()}")
+        binding.tvInt2Bytes.append("\nBE:Double -> Bytes:$dValue -> ${dValue.toByteArrayBE().contentToString()}")
         dBytes.reverse()
-        tvInt2Bytes.append("\nBE:Bytes -> Double:${dBytes.contentToString()} -> ${dBytes.toDoubleBE()}")
+        binding.tvInt2Bytes.append("\nBE:Bytes -> Double:${dBytes.contentToString()} -> ${dBytes.toDoubleBE()}")
 
         changeEntry(arrayOf("MainActivity", "Entry1", "Entry2"), 0)
-        btnSwichEntry.setOnClickListener {
+        binding.btnSwichEntry.setOnClickListener {
             changeEntry(arrayOf("MainActivity", "Entry1", "Entry2"), 0)
         }
 
-        btnDoubleClick.setOnDoubleClickListener {
+        binding.btnDoubleClick.setOnDoubleClickListener {
             "RepeatedClick".logE("Double Click!")
         }
 
-        btnTripleClick.setOnTripleClickListener {
+        binding.btnTripleClick.setOnTripleClickListener {
             "RepeatedClick".logE("Triple Click!")
         }
 
-        btnQuadrupleClick.setOnQuadrupleClickListener {
+        binding.btnQuadrupleClick.setOnQuadrupleClickListener {
             "RepeatedClick".logE("Quadruple Click!")
         }
 
-        btnPentaClick.setOnPentaClickListener {
+        binding.btnPentaClick.setOnPentaClickListener {
             "RepeatedClick".logE("Penta Click!")
         }
     }
